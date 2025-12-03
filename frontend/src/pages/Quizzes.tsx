bbeb6ad2471dbf2698fe79a756f37b63739cc3a8
XoDeR <xoder.vs@gmail.com>
2025-12-03 16:38:58 +0100

    tanstack query with example usage

--- frontend/src/pages/Quizzes.tsx
+++ frontend/src/pages/Quizzes.tsx
@@ -1,12 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { useQuery } from "@tanstack/react-query";
+import type { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
 export default function Quizzes() {
+  // tanstack query example usage
+  const { data, isLoading } = useQuery({
+    queryKey: ["quizzes"],
+    queryFn: async () => {
+      const res = await fetch("/api/quizzes");
+      return res.json();
+    },
+  });
+
   return (
     <>
       <h1>Quizzes</h1>
       <div className="flex min-h-svh flex-col items-center justify-center">
         <Button>Click me</Button>
       </div>
+      {/* tanstack query example usage */}
+      {isLoading ? <p>Loading...</p> : <ul>{data.map((q: { id: string; title: string; }) => <li key={q.id}>{q.title}</li>)}</ul>};
     </>
   );
 }
