6d7dd85547fe3e90b943c80f3c98b82f8a239bbe
XoDeR <xoder.vs@gmail.com>
2025-12-04 17:58:52 +0100

    added navigation to protected layout

--- frontend/src/layouts/ProtectedLayout.tsx
+++ frontend/src/layouts/ProtectedLayout.tsx
@@ -1,5 +1,20 @@
 import { Outlet, Navigate } from "react-router";
 
+import {
+  CircleUserRound,
+} from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { useNavigate } from 'react-router';
+
 // TODO replace with real auth check
 const isAuthenticated = () => {
   // TODO temp
@@ -12,10 +27,66 @@ export default function ProtectedLayout() {
     return <Navigate to="/login" replace />;
   }
 
+  // Items:
+  // submitted quizzes
+  // created quizzes
+  // separator
+  // profile
+  // separator
+  // logout
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // TODO implement logout
+  }
+
   return (
     <div className="protected-layout">
       <div className="w-full h-16 bg-gray-300">
-        <h1>Protected Area</h1>
+        <div className="w-full flex justify-end p-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon-lg" aria-label="Profile" className="rounded-full">
+                <CircleUserRound className="size-8" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-52">
+              <DropdownMenuGroup>
+                <DropdownMenuItem onClick={() => {
+                  const path = `/`;
+                  navigate(path);
+                }}>
+                  Submitted Quizzes
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => {
+                  const path = `/created-quizzes`;
+                  navigate(path);
+                }}>
+                  Created Quizzes
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem onClick={() => {
+                  const path = `/profile`;
+                  navigate(path);
+                }}>
+                  Profile
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem variant="destructive" onClick={() => {
+                  handleLogout();
+                }}>
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+
       </div>
 
       {/* Shared for protected pages */}
--- frontend/src/pages/quiz/QuizCreate.tsx
+++ frontend/src/pages/quiz/QuizCreate.tsx
@@ -1,3 +1,19 @@
 export default function QuizCreate() {
-  return <h1>QuizCreate</h1>;
+  return (
+    <div className="w-full">
+      <h1 className="text-3xl">Create New Quiz</h1>
+      <div className="p-20 border rounded-md">
+        <div>
+          <h3 className="text-xl">1. When JS was invented</h3>
+          <input type="radio" id="html" name="fav_language" value="HTML" />
+          <label htmlFor="html">HTML</label><br />
+          <input type="radio" id="css" name="fav_language" value="CSS" />
+          <label htmlFor="css">CSS</label><br />
+          <input type="radio" id="javascript" name="fav_language" value="JavaScript" />
+          <label htmlFor="javascript">JavaScript</label>
+        </div>
+
+      </div>
+    </div>
+  );
 }
