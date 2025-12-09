b796dbb6449db6ffb6461a38ef929a6702b26d82
XoDeR <xoder.vs@gmail.com>
2025-12-09 22:23:22 +0100

    Update authMiddleware.ts
    in auth middleware (for protected functions): extract cookie, attach user data (id, email) to response

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,17 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import "dotenv/config";
 
-const secret = "MY_SUPER_JWT_SECRET_MNBVCXZ";
+interface MyJwtPayload extends JwtPayload {
+  id: string;
+  email: string;
+}
+
 
 export const protect = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  const tokenFromCookie = req.cookies?.quiz_gen_access_token;
+  const token = tokenFromCookie;
   if (!token) {
-    return res.status(401).json({ error: "not authorized" });
+    return res.status(401).json({ message: 'No token provided' });
   }
 
   try {
-    const decoded = jwt.verify(token, secret);
-    (req as any).user = decoded;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as MyJwtPayload;
+    (req as any).user = { id: decoded.id, email: decoded.email };
     next();
   } catch (error) {
     res.status(401).json({ error: "Token is not valid" });
