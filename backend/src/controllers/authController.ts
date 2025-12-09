e18c8002120b602e73ec9f6f660623124da11232
XoDeR <xoder.vs@gmail.com>
2025-12-09 22:02:11 +0100

    removed unused

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -5,8 +5,6 @@ import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
 import "dotenv/config"; // does the same as import dotenv + call dotenv.config()
 
-const secret = "MY_SUPER_JWT_SECRET_MNBVCXZ";
-
 // Routes:
 // /register
 // /login
@@ -16,7 +14,7 @@ export const register = async (req: Request, res: Response) => {
   try {
     const { username, email, password } = req.body;
 
-    const existingUser = await sql`
+    const [existingUser] = await sql`
       SELECT id, username, email
       FROM users
       WHERE email = ${email}
@@ -37,27 +35,6 @@ export const register = async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Server error' });
   }
 }
-
-/*
-export const register = async (req: Request, res: Response) => {
-  try {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await prisma.user.create({
-      data: {
-        username,
-        password: hashedPassword,
-      },
-    });
-
-    const token = jwt.sign({ id: user.id }, secret, { expiresIn: "3h" });
-    res.status(201).json({ token });
-  } catch (error) {
-    res.status(400).json({ error: "Username already exists or unknown error" });
-  }
-};
-*/
 
 export const login = async (req: Request, res: Response) => {
   try {
@@ -83,28 +60,3 @@ export const login = async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Server error' });
   }
 }
-
-/*
-export const login = async (req: Request, res: Response) => {
-  try {
-    const { username, password } = req.body;
-    const user = await prisma.user.findUnique({
-      where: { username },
-    });
-
-    if (!user) {
-      return res.status(400).json({ error: "Invalid credentials" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ error: "Invalid credentials" });
-    }
-
-    const token = jwt.sign({ id: user.id }, secret, { expiresIn: "3h" });
-    res.json({ token });
-  } catch (error) {
-    res.status(500).json({ error: "Server error" });
-  }
-};
-*/
