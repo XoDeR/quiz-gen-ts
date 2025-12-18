bfd10edda96244af51e9c78add390e96ad857d1b
XoDeR <xoder.vs@gmail.com>
2025-12-18 21:09:49 +0100

    InProgressTab cleanup

--- frontend/src/pages/quizzes/InProgressTab.tsx
+++ frontend/src/pages/quizzes/InProgressTab.tsx
@@ -1,45 +1,14 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNavigate } from 'react-router';
 
-import { getInProgressColumns, type QuizInProgress } from "./InProgressColumns"
+import { getInProgressColumns } from "./InProgressColumns"
 import { DataTable } from "./InProgressDataTable";
 import { useUserSubmissions } from "@/hooks/useSubmissions";
-
-
-function getData(): Partial<QuizInProgress>[] {
-  return [
-    {
-      quizId: "1", // id of quiz
-      quizTitle: "Quiz 1", // title of quiz
-      submissionUpdatedAt: new Date(), // updated_at of submission
-    },
-    {
-      quizId: "2",
-      quizTitle: "Quiz 2",
-      submissionUpdatedAt: new Date(),
-    },
-    {
-      quizId: "3",
-      quizTitle: "Quiz 3",
-      submissionUpdatedAt: new Date(),
-    },
-    {
-      quizId: "4",
-      quizTitle: "Quiz 4",
-      submissionUpdatedAt: new Date(),
-    },
-  ];
-}
 
 export default function InProgressTab() {
   const navigate = useNavigate();
 
-  // get user's submissions with status completed === false
-  // TO BE IMPLEMENTED
-  // const { data: quizzes, isLoading } = useUserSubmissions(false);
   const { data: quizSubmissions, isLoading } = useUserSubmissions(false);
-
-  console.log("quizSubmissions: ", quizSubmissions);
 
   const handleOpenQuiz = (quizId: string) => {
     console.log(`Opening quiz with ID: ${quizId}`);
@@ -51,9 +20,6 @@ export default function InProgressTab() {
 
   const columns = getInProgressColumns(handleOpenQuiz);
 
-  // TO CHANGE
-  const mockData = getData();
-  
   return (
     <Card>
       <CardHeader>
