9e0589323ddaba2735c9bdf6e10b28d295f0d085
XoDeR <xoder.vs@gmail.com>
2025-12-22 11:15:36 +0100

    created required controllers

---
+++ backend-lr-concept/app/Http/Controllers/Api/V1/SubmissionController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Submission;
+use Illuminate\Http\Request;
+
+class SubmissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Submission $submission)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Submission $submission)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Submission $submission)
+    {
+        //
+    }
+}
